df33e723123068b2c93706fbf78f6f2087a75842
programmerjee <angelarowe7@gmail.com>
2017-09-07 19:27:04 -0400

    Code is mostly working

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,4 +1,48 @@
 
 public class VirtualPet {
 
+	public String name;
+	int interest = 5;
+	int hunger = 5;
+	int energy = 5;
+
+	public void showMenu() {
+		System.out.println("What would you like to do? \n\tPress 1 to play with " + name + "\n\tPress 2 to feed " + name
+				+ "\n\tPress 3 to make " + name + " sleep" + "\n\tPress 4 to show " + name + "'s status\n");
+	}
+
+	public void showStatus() {
+		System.out.println("\t" + name + "'s interest level is " + interest + "\n\t" + name + "'s hunger level is "
+				+ hunger + "\n\t" + name + "'s energy level is " + energy);
+	}
+
+	public void tick() {
+		interest -= 1;
+		hunger -= 1;
+		energy -= 1;
+	}
+
+	public void play() {
+		interest += 3;
+		energy -= 1;
+	}
+
+	public void feed() {
+		hunger += 3;
+		energy -= 1;
+	}
+
+	public void sleep() {
+		energy += 3;
+		interest -= 1;
+		hunger -= 1;
+	}
+
+	boolean myPetIsAlive() {
+		if (energy > 0 && interest > 0 && energy > 0) {
+			return true;
+		}
+		return false;
+
+	}
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,9 +1,45 @@
+import java.util.Scanner;
 
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		VirtualPet myPet = new VirtualPet();
+		myPet.name = "Panchu";
 
+		System.out.println("\t\tWelcome to Virtual Pet!  \n\tPanchu the Elephant needs your help!\n");
+		String userOption;
+		while (myPet.myPetIsAlive() == true) {
+			do {
+				myPet.showMenu();
+				userOption = input.next();
+			} while (!userOption.equals("1") && !userOption.equals("2") && !userOption.equals("3")
+					&& !userOption.equals("4"));
+			switch (userOption) {
+			case "1":
+				myPet.play();
+				myPet.tick();
+				System.out.println("Now " + myPet.name + " is excited!");
+				break;
+			case "2":
+				myPet.feed();
+				myPet.tick();
+				System.out.println("Now " + myPet.name + " is full!");
+				break;
+			case "3":
+				myPet.sleep();
+				myPet.tick();
+				System.out.println("Now " + myPet.name + " has lots of energy!");
+				break;
+			case "4":
+				myPet.tick();
+				myPet.showStatus();
+			}
+		}
+		if (myPet.myPetIsAlive() == false) {
+			System.out.println("Uh oh!  You killed " + myPet.name);
+			System.exit(0);
+		}
+		input.close();
 	}
-
 }
