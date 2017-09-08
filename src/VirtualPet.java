3d76b332a45eac564bb1080ab4d9d7cdcbe70496
programmerjee <angelarowe7@gmail.com>
2017-09-08 17:08:48 -0400

    added reason for death

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,25 +1,25 @@
+import java.util.Random;
 
 public class VirtualPet {
 
 	public String name;
-	int interest = 5;
-	int hunger = 5;
-	int energy = 5;
+	int interest = 10;
+	int hunger = 10;
+	int energy = 10;
+	int health = 10;
+	String [] exclamatoryMessage = {"Great job!", "Excellent!", "Fantastic!", "Right on!", "Alright!"};
 
-	public void showMenu() {
-		System.out.println("What would you like to do? \n\tPress 1 to play with " + name + "\n\tPress 2 to feed " + name
-				+ "\n\tPress 3 to make " + name + " sleep" + "\n\tPress 4 to show " + name + "'s status\n");
-	}
-
-	public void showStatus() {
-		System.out.println("\t" + name + "'s interest level is " + interest + "\n\t" + name + "'s hunger level is "
-				+ hunger + "\n\t" + name + "'s energy level is " + energy);
+	public String showMenu() {
+		return "What would you like to do? \n\tPress 1 to play with " + name + "\n\tPress 2 to feed " + name
+				+ "\n\tPress 3 to make " + name + " sleep" + "\n\tPress 4 to give " + name
+				+ " medicine\n\tPress 5 to see how " + name + " is doing\n";
 	}
 
 	public void tick() {
 		interest -= 1;
 		hunger -= 1;
 		energy -= 1;
+		health -= 1;
 	}
 
 	public void play() {
@@ -30,6 +30,7 @@ public class VirtualPet {
 	public void feed() {
 		hunger += 3;
 		energy -= 1;
+		health -= 2;
 	}
 
 	public void sleep() {
@@ -38,11 +39,45 @@ public class VirtualPet {
 		hunger -= 1;
 	}
 
+	public void medicine() {
+		health += 3;
+		interest -= 2;
+		energy -= 2;
+		hunger -= 2;
+	}
+
+	public String showStatus() {
+		return "\t" + name + "'s interest:  " + interest + "\n\t" + name + "'s satiety: " + hunger
+				+ "\n\t" + name + "'s energy level is " + energy + "\n\t" + name + "'s health: " + health;
+	}
+
 	boolean myPetIsAlive() {
-		if (energy > 0 && interest > 0 && energy > 0) {
+		if (energy > 0 && interest > 0 && energy > 0 && health > 0) {
 			return true;
 		}
 		return false;
-
 	}
-}
+
+	public String showPicture() {
+		return ("\t                      ____\n\t                .---'-    \\\n\t     .-----------/           \\\n\t     /           (         ^  |   __\n\t&   (             \\        O  /  / .'\n\t'._/(              '-'  (.   (_.' /\n\t     \\                    \\     ./\n\t      |    |       |    |/ '._.'\n\t       )   @).____\\|  @ |\n\t   .  /    /       (    |\n\t  \\|, '_:::\\  . ..  '_:::\\ ..\\)\n");
+	}
+	Random random = new Random();
+	int i =random.nextInt(exclamatoryMessage.length);
+	public String dangerZone() {
+		if (energy==1 || energy ==2) {return "Oh no!" +name + " is tired.";}
+		if (health==1 || health==2) {return "Oh no!" +name + " is sick.";}
+		if (interest==1 || interest ==2) {return "Oh no!" + name + " is bored.";}
+		if (hunger==1 || hunger==2) {return "Oh no!" + name + " is hungry.";}
+		else return exclamatoryMessage[i] ;
+		}
+	String causeOfDeath() {
+		if (energy<=0) {return "exhaustion.";}
+		if (health<=0) {return "disease.";}
+		if (interest<=0) {return "boredom.  Yes, it's possible!";}
+		if (hunger <=0) {return "starvation." ;}
+		else return name;
+		}
+	}
+//restart game
+//quit game
+//better warning
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -8,36 +8,45 @@ public class VirtualPetApp {
 		myPet.name = "Panchu";
 
 		System.out.println("\t\tWelcome to Virtual Pet!  \n\tPanchu the Elephant needs your help!\n");
+		System.out.println(myPet.showPicture());
 		String userOption;
 		while (myPet.myPetIsAlive() == true) {
 			do {
-				myPet.showMenu();
+				System.out.println(myPet.showMenu());
 				userOption = input.next();
 			} while (!userOption.equals("1") && !userOption.equals("2") && !userOption.equals("3")
-					&& !userOption.equals("4"));
+					&& !userOption.equals("4") && !userOption.equals("5"));
 			switch (userOption) {
 			case "1":
 				myPet.play();
 				myPet.tick();
 				System.out.println("Now " + myPet.name + " is excited!");
+				System.out.println(myPet.dangerZone());
 				break;
 			case "2":
 				myPet.feed();
 				myPet.tick();
 				System.out.println("Now " + myPet.name + " is full!");
+				System.out.println(myPet.dangerZone());
 				break;
 			case "3":
 				myPet.sleep();
 				myPet.tick();
 				System.out.println("Now " + myPet.name + " has lots of energy!");
+				System.out.println(myPet.dangerZone());
 				break;
 			case "4":
+				myPet.medicine();
 				myPet.tick();
-				myPet.showStatus();
+				System.out.println("Now " + myPet.name + " is feeling better.");
+				System.out.println(myPet.dangerZone());
+			case "5":
+				myPet.tick();
+				System.out.println(myPet.showStatus());
 			}
 		}
 		if (myPet.myPetIsAlive() == false) {
-			System.out.println("Uh oh!  You killed " + myPet.name);
+			System.out.println("Uh oh!  You killed " + myPet.name + "\n" + myPet.name + " died of " + myPet.causeOfDeath());
 			System.exit(0);
 		}
 		input.close();
